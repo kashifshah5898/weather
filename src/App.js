acc16f58e90a69e2bd24ef9ac4db1ddeb4adc889
kashif <kashifshah5898@gmail.com>
2022-06-29 11:09:14 +0500

    Add nackground css

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
+import React from "react";
 import "./App.css";
+import Routes from "./routes/routes";
+// import Routes from "./Routes/Routes";
 
 function App() {
   return (
-    <>
+    <div className="weatherAppBgcolor">
       <div className="container">
-        <h1>Hello World</h1>{" "}
-        <div className="jumbotron">
-          <h2>Jumbotron</h2>
-          <button className="btn btn-primary">Click me</button>
-        </div>
+        <Routes />
       </div>
-    </>
+    </div>
   );
 }
 
