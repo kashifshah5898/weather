580845b142b7fedc712a05a7a906af27dfaaaa5e
kashif <kashifshah5898@gmail.com>
2022-07-06 11:13:46 +0500

    Working on footer

--- src/components/navigation/footer/Footer.jsx
+++ src/components/navigation/footer/Footer.jsx
@@ -1,10 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
+import "../../styles/global.css";
 
 const Footer = () => {
+  const [email, setEmail] = useState("");
+
+  const subscribe = (e) => {
+    e.preventDefault();
+    alert("Functionality Remained at backend: " + email);
+  };
   return (
-    <>
-      <h1>Footer</h1>
-    </>
+    <div className="footer">
+      <div className="container mt-4">
+        <div className="row">
+          <div className="col-8">
+            <form className="d-flex" onSubmit={subscribe}>
+              <input
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder="Enter your email to subscribe"
+                type="email"
+                className="form-control responsive-font-example"
+                id="footerInput"
+                required
+              />
+              <input
+                className="btn btn-primary responsive-font-example "
+                id="footerSubmit"
+                type="submit"
+                value="Subscribe"
+                // onClick={subscribe}
+              />
+            </form>
+          </div>
+          <div className="col-4 d-flex">
+            <i class="fa-brands fa-facebook"></i>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.js";
+<link
+  rel="stylesheet"
+  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+></link>;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
