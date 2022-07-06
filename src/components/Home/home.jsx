b0004eec788725db23c1292ce02b50d6e171eaa9
kashif <kashifshah5898@gmail.com>
2022-07-06 20:35:21 +0500

    Getting axios error

--- src/components/Home/home.jsx
+++ src/components/Home/home.jsx
@@ -2,11 +2,13 @@ import React, { useState } from "react";
 import "../styles/global.css";
 import "./home.css";
 import { GrLocation } from "react-icons/gr";
+import { weatherAPI } from "../../api/service";
+import Toast from "../Toast/Toast";
 
 const Home = () => {
   const [location, setLocation] = useState("");
-  const [latitude, setLatitude] = useState("");
-  const [longitude, setLongitude] = useState("");
+  const [latLongAPI, setLatLongAPI] = useState("");
+  const [apiError, setAPiError] = useState(false);
 
   const findLocation = (e) => {
     e.preventDefault();
@@ -18,44 +20,60 @@ const Home = () => {
       navigator.geolocation.getCurrentPosition(showPosition);
     }
   };
-  const showPosition = (position) => {
-    setLatitude(position.coords.latitude);
-    setLongitude(position.coords.longitude);
+  const showPosition = async (position) => {
+    // try {
+    //   let apiResponse = await weatherAPI(position.coords.latitude, position.coords.longitude);
+    //   setLatLongAPI(apiResponse);
+    // } catch (error) {
+    //   setAPiError(true);
+    //   setTimeout(() => {
+    //     setAPiError(false);
+    //   }, 5000);
+    // }
   };
   return (
-    <div className="bgImage">
-      <div className="container">
-        <form className="d-flex p45" onSubmit={findLocation}>
-          <input
-            className="form-control responsive-font-example"
-            id="footerInput"
-            type="text"
-            placeholder="Find your location"
-            onChange={(e) => setLocation(e.target.value)}
-          />
-          &nbsp;
-          <span className="pointer" onClick={getLocation}>
-            <GrLocation className="locationIcon" />
-          </span>
-          &nbsp;
-          <input
-            className="btn btn-primary responsive-font-example "
-            id="footerSubmit"
-            type="submit"
-            value="find"
-          />
-        </form>
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
+    <>
+      {apiError ? <Toast /> : null}
+
+      <div className="bgImage">
+        <div className="container">
+          <form className="d-flex p45" onSubmit={findLocation}>
+            <input
+              className="form-control responsive-font-example"
+              id="footerInput"
+              type="text"
+              placeholder="Find your location"
+              onChange={(e) => setLocation(e.target.value)}
+            />
+            &nbsp;
+            <span className="pointer" onClick={getLocation}>
+              <GrLocation className="locationIcon" />
+            </span>
+            &nbsp;
+            <input
+              className="btn btn-primary responsive-font-example "
+              id="footerSubmit"
+              type="submit"
+              value="find"
+            />
+          </form>
+
+          <div
+            className="card mb-2"
+            style={{ width: "18rem", background: "#262936", height: "14rem" }}
+          >
+            <div className="card-body">
+              <h5 className="card-title color1">Card title</h5>
+              <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
+              <p className="card-text">
+                Some quick example text to build on the card title and make up the bulk of the
+                card's content.
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
