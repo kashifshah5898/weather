2de730e0210aace583a33261ab45855e083e56e7
kashif <kashifshah5898@gmail.com>
2022-07-06 16:08:22 +0500

    working on home component

--- src/components/Home/home.jsx
+++ src/components/Home/home.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import "../styles/global.css";
+import "./home.css";
+const Home = () => {
+  const [location, setLocation] = useState("");
 
-const home = () => {
-  return <>Home</>;
+  const findLocation = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <div className="bgImage">
+      <div className="container">
+        <form className="d-flex p45" onSubmit={findLocation}>
+          <input
+            className="form-control responsive-font-example"
+            id="footerInput"
+            type="text"
+            placeholder="Find your location"
+            onChange={(e) => setLocation(e.target.value)}
+          />
+          <input
+            className="btn btn-primary responsive-font-example "
+            id="footerSubmit"
+            type="submit"
+            value="find"
+          />
+        </form>
+      </div>
+    </div>
+  );
 };
 
-export default home;
+export default Home;
